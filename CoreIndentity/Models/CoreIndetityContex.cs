fdebe7e11f3d54fec11e3117a290adf8ab88c35e
Michael Karafir <blackmanskill@gmail.com>
2018-09-22 14:29:31 +0900

    Add files via upload

---
+++ CoreIndentity/Models/CoreIndetityContex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreIndentity.Models
+{
+    public class CoreIndetityContex : DbContext
+    {
+        public CoreIndetityContex(DbContextOptions<CoreIndetityContex> options)
+            : base(options) { }
+
+        public DbSet<urusan> urusans { get; set; }
+    }
+}
---
+++ CoreIndentity/Models/urusan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreIndentity.Models
+{
+    public class urusan
+    {
+        public int id { get; set; }
+        public string kode { get; set; }
+        public string namaurusan { get; set; }
+    }
+}
